a573ed143d0efb6728cce8a5bef1f2f009d043c5
zyy <273971005@qq.com>
2018-08-18 00:11:02 -0700

    fib,fac,sum by thread
    Signed-off-by: zyy <273971005@qq.com>

--- mtfacfib.py
+++ mtfacfib.py
@@ -1,47 +1,55 @@
 #!/usr/bin/env python
-'''mtfacfib
+"""mtfacfib
     fabnacci N! and sum using thread
-'''
-import threading
+"""
+from MyThread import MyThread
 from time import sleep, ctime
 
-loops = (4, 2)
+
+def fib(x):
+    sleep(0.005)
+    if x < 2: return 1
+    return (fib(x - 2), +fib(x - 1))
 
 
-class MyThread(threading.Thread):
-    def __init__(self, func, args, name=''):
-        threading.Thread.__init__(self)
-        self.name = name
-        self.func = func
-        self.args = args
-
-    def run(self):
-        apply(self.func, self.args)
+def fac(x):
+    sleep(0.1)
+    if x < 2: return 1
+    return (x * fac(x - 1))
 
 
-def loop(nloop, nsec):
-    print 'start loop', nloop, 'at:', ctime()
-    sleep(nsec)
-    print 'loop', nloop, 'done at:', ctime()
+def sum(x):
+    sleep(0.1)
+    if x < 2: return 1
+    return (x + sum(x - 1))
+
+
+funcs = [fib, fac, sum]
+n = 12
 
 
 def main():
-    print 'starting at:', ctime()
-    threads = []
-    nloops = range(len(loops))
+    nfuncs = range(len(funcs))
 
-    for i in nloops:
-        t = MyThread(loop, (i, loops[i]),
-                     loop.__name__)
+    print '*** SINGLE THREAD'
+    for i in nfuncs:
+        print 'starting', funcs[i].__name__, 'at:', ctime()
+        print funcs[i](n)
+        print funcs[i].__name__, 'finished at:', ctime()
+    print '\n*** MULTIPLE THREADS'
+    threads = []
+    for i in nfuncs:
+        t = MyThread(funcs[i], (n,), funcs[i].__name__)
         threads.append(t)
 
-    for i in nloops:
+    for i in nfuncs:
         threads[i].start()
 
-    for i in nloops:
+    for i in nfuncs:
         threads[i].join()
+        print threads[i].getResult()
 
-    print 'all DONE at:', ctime()
+    print 'all Done'
 
 
 if __name__ == '__main__':
